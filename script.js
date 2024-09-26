fa70dc9facbe2407fe7712cf36ba4cff939f0ecc
Yura <yura.tihomirov@rambler.ru>
2024-09-26 19:17:28 +0400

    Поправил баннер, текстовый блок (Значение) заголовки, наложение баннера

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let textModuleHeight = modules[2].offsetHeight;
 let firstModules = firstModuleHeight + secondModuleHeight
 
 let currentWordIndex = 0;
-let textOffset = (window.innerHeight * 0.04)
+let textOffset = (window.innerHeight * 0.4)
 let textScroll = textModuleHeight - window.innerHeight
 let scrollPerWord = textScroll / word.length
 let startTextScroll = firstModules - textOffset
@@ -37,60 +37,6 @@ window.onscroll = function() {
 }
 })
 
-/* const switchStudy = document.getElementById('switchStudy');
-const switchWork = document.getElementById('switchWork');
-const switchLife = document.getElementById('switchLife');
-
-const textStudy = document.getElementById('textStudy');
-const textWork = document.getElementById('textWork');
-const textLife = document.getElementById('textLife');
-
-const imgStudy = document.getElementById('imgStudy');
-const imgWork = document.getElementById('imgWork');
-const imgLife = document.getElementById('imgLife');
-
-switchStudy.onclick = () => {
-    switchStudy.classList.add('active'),
-    switchWork.classList.remove('active'),
-    switchLife.classList.remove('active'),
-
-    textStudy.classList.add('visible'),
-    textWork.classList.remove('visible'),
-    textLife.classList.remove('visible')
-
-    imgStudy.classList.add('visible'),
-    imgWork.classList.remove('visible'),
-    imgLife.classList.remove('visible')
-}
-
-switchWork.onclick = () => {
-    switchWork.classList.add('active'),
-    switchStudy.classList.remove('active'),
-    switchLife.classList.remove('active'),
-
-    textWork.classList.add('visible'),
-    textStudy.classList.remove('visible'),
-    textLife.classList.remove('visible'),
-
-    imgWork.classList.add('visible'),
-    imgStudy.classList.remove('visible'),
-    imgLife.classList.remove('visible')
-}
-
-switchLife.onclick = () => {
-    switchLife.classList.add('active'),
-    switchStudy.classList.remove('active'),
-    switchWork.classList.remove('active'),
-
-    textLife.classList.add('visible'),
-    textWork.classList.remove('visible'),
-    textStudy.classList.remove('visible'),
-
-    imgLife.classList.add('visible'),
-    imgStudy.classList.remove('visible'),
-    imgWork.classList.remove('visible')
-} */
-
     const firstSwitches = {
         study: {
             button: document.getElementById('switchStudy'),
@@ -108,21 +54,6 @@ switchLife.onclick = () => {
             img: document.getElementById('imgLife')
         },
     };
-
-  /*   const setFirstActive = (activeKey) => {
-        for (const key in firstSwitches) {
-            const { button, text, img } = firstSwitches[key];
-            if (key === activeKey) {
-                button.classList.add('active');
-                text.classList.add('visible');
-                img.classList.add('visible');
-            } else {
-                button.classList.remove('active');
-                text.classList.remove('visible');
-                img.classList.remove('visible');
-            }
-        }
-    }; */
     
        const secondSwitches = {
         help: {
@@ -142,20 +73,6 @@ switchLife.onclick = () => {
         }
     }
     
-    /* const setSecondActive = (activeKey) => {
-        for (const key in secondSwitches) {
-            const { button, text, img } = secondSwitches[key];
-            if (key === activeKey) {
-                button.classList.add('active');
-                text.classList.add('visible');
-                img.classList.add('visible');
-            } else {
-                button.classList.remove('active');
-                text.classList.remove('visible');
-                img.classList.remove('visible');
-            }
-        }
-    }; */
     const setActive = (switches, activeKey) => {
         for (const key in switches) {
             const { button, text, img } = switches[key];
@@ -181,19 +98,14 @@ switchLife.onclick = () => {
 
     function onToggle(event) {
         if (event.target.open) {
-          document.querySelectorAll(".FAQ__answers_item > details[open]").forEach((el) => {
-            // Исключаем из перебора елемент который мы только что открыли
+          document.querySelectorAll(".FAQ__answers_item > details[open]").forEach((el) => {      
             if (el === event.target) {
               return;
             }
-      
-            // Закрываем все остальные елементы <details>
             el.open = false;
           });
         }
       }
-      
-      // Вешаем наблюдатель на все елементы <details> внутри акордиона
       document
         .querySelectorAll(".FAQ__answers_item > details")
         .forEach((el) => el.addEventListener("toggle", onToggle));
